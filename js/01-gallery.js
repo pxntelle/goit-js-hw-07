9a0983fa0716aefc5329e35f8a1c712847c7cf9b
pxntelle <taniapantelle@gmail.com>
2023-05-26 18:11:52 +0200

    correct basiclightbox way indicated

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,7 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-import basicLightbox from "basiclightbox";
+import basicLightbox from "node_modules/basiclightbox/dist/basicLightbox.min.js";
 
 console.log(galleryItems);
 
